f0fedcad44fbeca4feef9860ece38e190a425269
finick5210 <finick5210@yandex.ru>
2020-07-26 01:06:47 +0300

    Added .eslintignore, added handler for case with incorrect data file path

--- routes/cards.js
+++ routes/cards.js
@@ -1,12 +1,15 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const router = require('express').Router();
 
 router.get('/', (req, res) => {
-  const filePath = path.join('./data/cards.json');
-  const reader = fs.createReadStream(filePath, { encoding: 'utf8' });
-
-  reader.pipe(res);
+  fs.readFile(path.join('./data/cards.json'), 'utf8')
+    .then((cards) => {
+      res.send(cards);
+    })
+    .catch(() => {
+      res.status('500').send();
+    });
 });
 
 module.exports = router;
--- routes/users.js
+++ routes/users.js
@@ -1,32 +1,36 @@
-const fs = require('fs');
+const fs = require('fs').promises;
 const path = require('path');
 const router = require('express').Router();
 
 const PATH = './data/users.json';
 
 router.get('/', (req, res) => {
-  const filePath = path.join(PATH);
-  const reader = fs.createReadStream(filePath, { encoding: 'utf8' });
-
-  reader.pipe(res);
+  fs.readFile(path.join(PATH), 'utf8')
+    .then((users) => {
+      res.send(users);
+    })
+    .catch(() => {
+      res.status('500').send();
+    });
 });
 
 router.get('/:id', (req, res) => {
-  fs.readFile(path.join(PATH), { encoding: 'utf8' }, (err, users) => {
-    if (err) {
-      return;
-    }
+  fs.readFile(path.join(PATH), 'utf8')
+    .then((users) => {
+      // eslint-disable-next-line no-underscore-dangle
+      const foundUser = JSON.parse(users).find((user) => user._id === req.params.id);
 
-    /* eslint no-underscore-dangle: ["error", { "allow": ["_id"] }] */
-    const foundUser = JSON.parse(users).find((user) => user._id === req.params.id);
+      if (!foundUser) {
+        res.status('404');
+        res.send({ message: 'Нет пользователя с таким id' });
+        return;
+      }
 
-    if (!foundUser) {
-      res.status('404');
-      res.send({ message: 'Нет пользователя с таким id' });
-      return;
-    }
-    res.send(foundUser);
-  });
+      res.send(foundUser);
+    })
+    .catch(() => {
+      res.status('500').send();
+    });
 });
 
 module.exports = router;
